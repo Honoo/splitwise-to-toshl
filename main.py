cbd6f65610a545ac424724c740f1b13066ed5452
Jia Xun <chaijiaxun@gmail.com>
2020-12-21 23:21:22 +0800

    Added pagination to categories and tags

--- main.py
+++ main.py
@@ -65,13 +65,12 @@ def get_toshl_cats_and_tags():
 
 clear()
 print("Splitwise to Toshl expense transfer tool\n")
-print("\n")
 print("Loading config... ")
 
 config = configparser.RawConfigParser()
 config.read('config.cfg')
 auth_dict = dict(config.items('API_KEYS'))
-print("done\n")
+print("done")
 splitwise_headers = {"content-type":"application/json", "Authorization": "Bearer " + auth_dict['splitwise_api_key']}
 toshl_headers = {"content-type":"application/json", "Authorization": "Bearer " + auth_dict['toshl_api_key']}
 
@@ -154,15 +153,19 @@ print("success")
 
 print('Successfully authenticated with both services.')
 print(f'  Splitwise account: {user_accounts["splitwise"]["email"]} ({user_accounts["splitwise"]["id"]})' )
-# print(f'  Toshl account: {user_accounts["toshl"]["email"]} ({user_accounts["toshl"]["id"]})' )
-# print("\n")
-# input("Press Enter to continue...")
+print(f'  Toshl account: {user_accounts["toshl"]["email"]} ({user_accounts["toshl"]["id"]})' )
+print("\n")
+input("Press Enter to continue...")
 
 while (True):
-  # clear()
+  clear()
+  print("Splitwise to Toshl expense transfer tool\nMain Menu\n")
+  print(f'  Splitwise account: {user_accounts["splitwise"]["email"]} ({user_accounts["splitwise"]["id"]})' )
+  print(f'  Toshl account: {user_accounts["toshl"]["email"]} ({user_accounts["toshl"]["id"]})' )
+  print("")
   print("What would you like to do now?")
-  print("1. Transfer from Friends List")
-  print("0. Quit")
+  print("[1] Transfer from Friends List")
+  print("[0] Quit")
   i = input()
   if (i == '1'):
     clear()
@@ -178,7 +181,7 @@ while (True):
     for f in friends_arr:
       if ( len(f['balance']) > 0):
         full_name = ', '.join(filter(None, (f['first_name'], f['last_name'])))
-        print(str(ind) + '. ' + full_name + ' ' + f['balance'][0]['amount'] + f['balance'][0]['currency_code'])
+        print(f"[{str(ind)}] {full_name} {f['balance'][0]['amount']} {f['balance'][0]['currency_code']}")
         friends_with_balances.append(f)
         ind = ind +1
 
@@ -214,9 +217,10 @@ while (True):
 
       print("")
       print("Choose an option")
-      print("1. Start processing here (you can skip individual expenses)")
-      print("2. Load next page")
-      print("0. Back to main menu")
+      print("[1] Start processing (you can skip individual expenses)")
+      print("[2] Next page")
+      print("[3] Prev page")
+      print("[0] Back to main menu")
 
       i = input()
       if (i == '1'):
@@ -254,14 +258,141 @@ while (True):
           print("")
           # Give options to add, or skip
           print("Choose an option")
-          print("1. Add expense (you need to provide the category and tags)")
-          print("2. Skip expense")
-          print("0. Finish this page and go back")
+          print("[1] Add expense (you need to provide the category and tags)")
+          print("[2] Next expense")
+          print("[0] Finish this page and go back")
 
           i = input()
           if (i == '1'):
-            # Get toshl categories
-            # Get toshl tags
+            bail = False
+            finish = False
+            offset = 0
+            page_size = 10
+            while(True): # Paginate category list
+              clear()
+              print(f"Friend Transfer > {full_name} > Page {page} > Item {expense_ind} of {len(expenses)} > Add Expense > Choose Category") 
+              print(f"==========================================") 
+              print("")
+              print("Adding expense:")
+              print(splitwise_expense_long_string(e))
+              print("")
+              print(f"Choose your category (showing {offset+1} to {offset+page_size+1}")
+              
+              ind = 0
+              toshl_categories_slice = toshl_categories[offset:offset+page_size]
+              for c in toshl_categories_slice:
+                print(f"[{ind}] {c['name']}")
+                ind += 1
+              print("")
+              print("[<] Prev page        [>] Next page")
+              print(f"[b] Back to {full_name}'s expenses")
+
+              selected_category = None
+              while(True): # collect user input
+                i = input()
+                if i == ">":
+                  offset += page_size
+                  if offset > len(toshl_categories) - page_size:
+                    offset = max(0, len(toshl_categories) - page_size)
+                  break # Break out of input loop
+                elif i == "<":
+                  offset -= page_size
+                  if offset < 0:
+                    offset = 0
+                  break # Break out of input loop
+                elif i == 'b':
+                  bail = True
+                  break # Break out of input loop
+                elif int(i) < 0 or int(i) >= len(toshl_categories_slice):
+                  print('Invalid input!')
+                  input("Press Enter to try again")
+                else:
+                  selected_category = toshl_categories_slice[int(i)]
+                  finish = True
+                  break # Break out of input loop
+
+              if bail or finish:
+                break # Break out of category page loop
+            if bail:
+              break  # Break out of expense page loop
+
+            offset = 0
+            page_size = 15
+            bail = False
+            finish = False
+            while (True): # Paginate tags list
+              clear()
+              print(f"Friend Transfer > {full_name} > Page {page} > Item {expense_ind} of {len(expenses)} > Add Expense > Choose Tag") 
+              print(f"==========================================") 
+              print("")
+              print("Adding expense:")
+              print(splitwise_expense_long_string(e))
+              print("")
+              print(f"Category: {selected_category['name']}")
+              print(f"Choose your tag (showing {offset+1} to {offset+page_size+1})\n(You can only choose one, sorry. To add more use the app.s)")
+              ind = 0
+              selected_category_tags = selected_category['tags']
+              category_tags_slice = selected_category_tags[offset:offset+page_size]
+              print("[n] <no tag>")
+              for t in category_tags_slice:
+                print(f"[{ind}] {t['name']}")
+                ind += 1
+              print("")
+              print("[<] Prev page        [>] Next page")
+              print(f"[b] Back to {full_name}'s expenses")
+              
+              selected_tag = None
+              while(True): # Collect user input
+                i = input()
+                if i == ">":
+                  offset += page_size
+                  if offset > len(selected_category_tags) - page_size:
+                    offset = max(0, len(selected_category_tags) - page_size)
+                  break # Break out of input loop
+                elif i == "<":
+                  offset -= page_size
+                  if offset < 0:
+                    offset = 0
+                  break # Break out of input loop
+                elif i == 'n':
+                  finish = True
+                  break # Break out of input loop
+                elif i == 'b':
+                  bail = True
+                  break # Break out of input loop
+                elif int(i) < 0 or int(i) >= len(category_tags_slice):
+                  print('Invalid input!')
+                  input("Press Enter to try again")
+                else:
+                  selected_tag = category_tags_slice[int(i)]
+                  finish = True
+                  break # Break out of input loop
+              if bail or finish:
+                break # Break out of category page loop
+
+            if bail:
+              break # Break out of expense page loop
+
+            # Add the entry to toshl
+            data = {
+              "amount": -abs(float(e['share_amount'])),
+              "currency": { "code": e['currency'] },
+              "date": e['date'],
+              "desc": e['description'],
+              "category": selected_category['id']
+            }
+            if selected_tag is not None:
+              data['tags'] = [selected_tag['id']]
+            r = requests.post(f"https://api.toshl.com/entries", data=json.dumps(data), headers=toshl_headers)
+            if r.status_code == 201:
+              print("Successfully added expense:")
+              print(toshl_entry_short_string(data))
+            else:
+              print("Could not add expense:")
+              print(f"Status: {r.status_code}")
+              print(r.text)
+
+            input("Press Enter to continue")
             expense_ind += 1
             continue
           elif (i == '2'):
@@ -279,15 +410,18 @@ while (True):
       elif (i == '2'):
         page += 1
         continue
+      elif (i == '3'):
+        page -= 1
+        if page < 0:
+          page = 0
+        continue
       elif (i == '0'):
         break
       else:
         print('Invalid input!')
-        input("Press Enter to try again")
+        input("Press Enter to go to next expense")
 
     
-
-      
   elif (i == '0'):
     exit()
   else:
